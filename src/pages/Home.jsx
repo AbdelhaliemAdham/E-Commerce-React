3dfb71dbc0ac04070358fbe6b565a5248d41fea6
abdo09adham@gmail.com <abdo09adham@gmail.com>
2025-01-20 09:48:03 +0200

    Banner and tailwind added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HomeView from "../components/HomeView";
 import NavBar from "../components/NavBar";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <HomeView />
+      <Banner />
     </>
   );
 }
