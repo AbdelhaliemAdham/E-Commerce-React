6f04f01c74c10b59c49c84194062f37c8bc11e5d
abdo09adham@gmail.com <abdo09adham@gmail.com>
2025-01-22 17:14:03 +0200

    nav bar became much responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,6 +23,17 @@ export default function NavBar() {
   return (
     <nav className={navbarClass} data-bs-theme={dataTheme}>
       <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <img src={img} alt="logo" />
         <a className="navbar-brand m-1" href="/">
           <span
@@ -35,7 +46,7 @@ export default function NavBar() {
           className="collapse navbar-collapse mt-2"
           id="navbarSupportedContent"
         >
-          <ul className="navbar-nav me-auto">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link
                 className="nav-link active bg-blue"
